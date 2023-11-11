7c49775c5a3c7724e605d99a6b61608d5a8be2f5
Neil Sawhney <Neil@neil-sawhney.com>
2023-11-10 21:43:45 -0500

    lots of stuff

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contact@markups.io";
+        $recipient = "Jules.e.r@outlook.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
